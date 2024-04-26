6c036879350c1045d03eb07b3441a2c0ad25c0f6
Paul Cahill <paulcahill97@outlook.com>
2024-04-26 23:05:06 +0100

    defining the column names, checking types of data, getting a summary of the data

--- analysis.py
+++ analysis.py
@@ -4,6 +4,16 @@
 # libraries
 import pandas as pd
 
-# importing the dataset from a URL
+# importing the dataset
 df = pd.read_csv("iris.data")
 print(df)
+
+# naming the columns
+columns = ['sepal length (cm)','sepal width (cm)','petal length (cm)','petal width (cm)','class']
+df.columns = columns
+
+# Checking the types of data held in the dataset
+print("Types of data:", df.dtypes)
+
+# summary of data
+print (df.describe())
